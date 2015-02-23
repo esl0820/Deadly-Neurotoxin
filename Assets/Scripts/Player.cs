b86cef4c24c286780b5de21f20d83069f9bfcb4d
Eric Leung <esl361@nyu.edu>
2015-02-23 12:24:46 -0500

    Something

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 200f;
+	public Text uiText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward * speed * Time.deltaTime;
+			uiText.text = "I'm moving forward!";
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= transform.forward *  speed * Time.deltaTime;
+			uiText.text = "I'm moving backwards!";
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (0f, -30f  * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0f, 30f  * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.position += transform.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			transform.position -= transform.up * speed * Time.deltaTime;
+		}
+	
+	}
+}
